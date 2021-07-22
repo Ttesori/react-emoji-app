de5b35ad170b833a9a1478f667c45acc7de21b1d
Toni Tesori <tt@tesoridesign.com>
2021-07-22 08:35:21 -0400

    Added border to header and footer

--- src/pages/layouts/Main.js
+++ src/pages/layouts/Main.js
@@ -19,7 +19,7 @@ export default function Main({ children }) {
   }
   return (
     <>
-      <header className="bg-blue-300 p-5  dark:bg-blue-800 dark:text-white mb-5 sm:mb-7">
+      <header className="bg-blue-300 p-5  dark:bg-blue-800 dark:text-white mb-5 sm:mb-7 border border-b-4 border-blue-200 dark:border-blue-900">
         <div className="container flex flex-col sm:flex-row justify-between items-center max-w-screen-lg mx-auto">
           <h1 className="text-2xl sm:text-xl font-bold text-blue-900 dark:text-blue-50 mb-2 sm:mb-0"><span className="text-3xl mr-1">🕵️‍♀️ </span> EmojiHunt</h1>
           <nav>
@@ -35,7 +35,7 @@ export default function Main({ children }) {
         {children}
       </main>
       <ToastContainer />
-      <footer className="bg-blue-300 p-5  dark:bg-blue-800 dark:text-blue-400 mt-3">
+      <footer className="bg-blue-300 p-5  dark:bg-blue-800 dark:text-blue-400 mt-3 border border-t-4 border-blue-200 dark:border-blue-900">
         <p className="font-bold text-center text-sm uppercase tracking-wide"><a href="https://tonitesori.dev" className="text-blue-800 dark:text-blue-300  dark:hover:text-blue-200">React Development by Toni</a>  🐧 </p>
       </footer>
     </>
